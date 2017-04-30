3a40ccf438a8cac645119cf8221b9ba2e81ff645
Helon <moreirahelon@gmail.com>
2017-04-30 18:00:18 -0300

    Sprint 2
    Foi feito a parte de realizar impréstimo e a devolução.
    
    Testes rodando corretamente

--- src/main/java/HELONMOREIRA_QUESTAO8/Biblioteca.java
+++ src/main/java/HELONMOREIRA_QUESTAO8/Biblioteca.java
@@ -62,4 +62,90 @@ public class Biblioteca {
 		user.SetTempoBloqueado(tempo);
 		BD_Usuario.add(user);
 	}
+	
+	//---------------------------------------------------------//
+	boolean EmprestarLivro(String nome){
+		Usuario user = null;
+		
+		for(Usuario u : BD_Usuario){
+			if(u.GetNome() == nome){
+				user = u;
+			}
+		}
+		
+		if(user == null){
+			return false;
+		}
+		
+		return user.GetUsuarioBloqueado();
+	}
+	Livro ProcurarLivro(String nome){
+		
+		System.out.printf("Procurando o livro %s! \n", nome);
+		for(Livro l : BD_Livros){
+			if(l.GetNome() == nome){
+				System.out.printf("Livro encontrado! \n");
+				return l;
+			}
+		}
+		System.out.printf("Livro nao encontrado! \n");
+		return null;
+	}
+	
+	void RegistrarEmprestimo(String nomeLivro, String nomeUsuario,int tempo){
+		Livro book;
+		Usuario user = null;
+		
+		book = ProcurarLivro(nomeLivro);
+		
+		if (book == null){
+			return;
+		}
+		
+		for(Usuario u : BD_Usuario){
+			if(u.GetNome() == nomeUsuario){
+				user = u;
+			}
+		}
+		
+		if(user == null){
+			return ;
+		}
+		
+		Sistema system = new Sistema(nomeLivro,nomeUsuario,tempo);
+		
+		BD_Livros.remove(book);
+		book.SetSituacaoDoLivro("Retirado");
+		BD_Livros.add(book);
+		HistoricoDeEmprestimos.add(system);
+	}
+	
+	void RegistrarDevolucao(String nomeLivro, String nomeUsuario){
+		Livro book;
+		Usuario user = null;
+		
+		book = ProcurarLivro(nomeLivro);
+		
+		if (book == null){
+			return;
+		}
+		
+		for(Usuario u : BD_Usuario){
+			if(u.GetNome() == nomeUsuario){
+				user = u;
+			}
+		}
+		
+		if(user == null){
+			return ;
+		}
+		
+		Sistema system = new Sistema(nomeLivro, nomeUsuario, 0);
+		
+		BD_Livros.remove(book);
+		book.SetSituacaoDoLivro("Disponível");
+		BD_Livros.add(book);
+		
+		HistoricoDeEmprestimos.remove(system);
+	}
 }
---
+++ src/test/java/HELONMOREIRA_QUESTAO8/Sprint_2Test.java
@@ -0,0 +1,38 @@
+package HELONMOREIRA_QUESTAO8;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Sprint_2Test {
+
+	@Test
+	public void testRegistrarEmprestimo() {
+		Biblioteca _Biblioteca = new Biblioteca();
+		Usuario _Usuario = new Usuario("UsuarioTeste",true,0);
+		Livro _Livro = new Livro("LivroTeste","Disponível");
+		
+		_Biblioteca.BD_Livros.add(_Livro);
+		_Biblioteca.InserirNovoUsuario("UsuarioTeste");
+		_Biblioteca.RegistrarEmprestimo("LivroTeste","UsuarioTeste",18);
+		
+		assertEquals("Retirado", _Biblioteca.BD_Livros.get(0).GetSituacaoDoLivro());
+	}
+	
+	@Test
+	public void testRegistrarDevolucaoLivro(){
+		Biblioteca _Biblioteca = new Biblioteca();
+		Usuario _Usuario = new Usuario("UsuarioTeste",true,0);
+		Livro _Livro = new Livro("LivroTeste","Disponível");
+		
+		_Biblioteca.BD_Livros.add(_Livro);
+		_Biblioteca.InserirNovoUsuario("UsuarioTeste");
+		_Biblioteca.RegistrarEmprestimo("LivroTeste","UsuarioTeste",18);
+		_Biblioteca.RegistrarDevolucao("LivroTeste","UsuarioTeste");
+		
+		assertEquals("Disponível", _Biblioteca.BD_Livros.get(0).GetSituacaoDoLivro());
+	}
+
+}
